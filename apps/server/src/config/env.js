0e8eed81e0c1631e09a60cd605133a1e5009b94e
Sudip Sharma <enix.xis@gmail.com>
2025-11-07 18:06:13 -0500

    removed env dummy

--- apps/server/src/config/env.js
+++ apps/server/src/config/env.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 4000,
-  mongoUri:
-    process.env.MONGODB_URI ||
-    'mmongodb+srv://event-hub:event-hub@event-hub.uttthob.mongodb.net/event-hub',
-  jwtSecret: process.env.JWT_SECRET || 'dev_secret',
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
+  port: process.env.PORT,
+  mongoUri: process.env.MONGODB_URI,
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN,
 };
